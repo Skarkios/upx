7c9079bc089f1a84a6780489db4bdf889dd976d1
Markus F.X.J. Oberhumer <markus@oberhumer.com>
2026-01-06 01:00:19 +0100

    all: switch to yearless copyright

--- src/stub/scripts/bin2h.py
+++ src/stub/scripts/bin2h.py
@@ -5,7 +5,7 @@
 #
 #  This file is part of the UPX executable compressor.
 #
-#  Copyright (C) 1996-2025 Markus Franz Xaver Johannes Oberhumer
+#  Copyright (C) Markus Franz Xaver Johannes Oberhumer
 #  All Rights Reserved.
 #
 #  UPX and the UCL library are free software; you can redistribute them
@@ -49,9 +49,9 @@ def w_header_c(w, ifile, ofile, n):
     w("""\n\
    This file is part of the UPX executable compressor.
 
-   Copyright (C) 1996-2025 Markus Franz Xaver Johannes Oberhumer
-   Copyright (C) 1996-2025 Laszlo Molnar
-   Copyright (C) 2000-2025 John F. Reiser
+   Copyright (C) Markus Franz Xaver Johannes Oberhumer
+   Copyright (C) Laszlo Molnar
+   Copyright (C) John F. Reiser
    All Rights Reserved.
 
    UPX and the UCL library are free software; you can redistribute them
